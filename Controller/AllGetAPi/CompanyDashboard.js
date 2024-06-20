dedb2fb6399d3af86516549735d438b2a79bd0b5
fahadsws <159604562+fahadsws@users.noreply.github.com>
2024-06-20 12:42:06 +0530

    Update CompanyDashboard.js

--- Controller/AllGetAPi/CompanyDashboard.js
+++ Controller/AllGetAPi/CompanyDashboard.js
@@ -1,27 +1,40 @@
-const pool= require('../../DB/Database')
+const pool = require('../../DB/Database');
+const bids = require("./UsersBid");
 
 
-async function Dashboard(req,res){
-    try{
-        const {id} = req.params;
+async function Dashboard(req, res) {
+    try {
+        const { id } = req.params;
 
-        pool.query(`Select * from payement where created_to = ?`,[id],async (err,result)=>{
+        pool.query(`Select * from payement where created_to = ?`, [id], async (err, result) => {
             if (err) {
                 console.error('Error executing second SQL query:', err);
                 return res.status(500).json({ error: 'Internal Server Error' });
             }
 
-            let totalPrice = 0;
-            await result.forEach(row => {
-                totalPrice += row.price;
+
+            const bidIds = await result.map(bid => bid.price);
+            console.log(bidIds)
+
+            pool.query(`SELECT * FROM seller_bid WHERE id IN (?)`, [bidIds], async (err, bids) => {
+                if (err) {
+                    console.error('Error executing second SQL query:', err);
+                    return res.status(500).json({ error: 'Internal Server Error' });
+                }
+
+                let totalPrice = 0;
+                await bids.forEach(row => {
+                    totalPrice += row.price;
+                });
+
+                res.status(200).json({ status: 200, total: totalPrice, sales: result?.length })
             });
 
-            res.status(200).json({status:200,total:totalPrice,sales:result?.length})
 
         })
 
-    }catch(e){
-        res.stats(500).json({message:'Internal Server Error'})
+    } catch (e) {
+        res.stats(500).json({ message: 'Internal Server Error' })
     }
 }
 
